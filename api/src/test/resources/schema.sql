c56b4bdf9b8c3ff0646f13003a5f9f0f5b6223a2
humdynsys <humdynsys@protonmail.com>
2024-10-20 20:33:04 -0400

    updated tests schema and inserts

--- api/src/test/resources/schema.sql
+++ api/src/test/resources/schema.sql
@@ -1,18 +1,15 @@
 CREATE TYPE subscription_level_enum AS ENUM ('FREE', 'STARTER', 'RESEARCH', 'ENTERPRISE');
 
 CREATE TABLE Actor (
-uid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-first_name VARCHAR(100) NOT NULL,
-last_name VARCHAR(100) NOT NULL,
-email VARCHAR(255) NOT NULL,
-phone VARCHAR(20) NOT NULL,
-subscription_level subscription_level_enum
-);
-
-CREATE TABLE Node (
-uid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-name VARCHAR(255) NOT NULL,
-payload JSONB NOT NULL,
-ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-actor_uuid UUID
+    uid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email_primary VARCHAR(255) NOT NULL,
+    email_secondary VARCHAR(255),
+    phone VARCHAR(20) NOT NULL,
+    subscription_level subscription_level_enum DEFAULT 'FREE',
+    node_name VARCHAR(255),
+    node_payload JSONB,
+    node_ts TIMESTAMP,
+    node_uid UUID
 );
