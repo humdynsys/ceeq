24f40f5d44961967ee7df3e25b9c9a931170e7fe
HumDynCen <humdyncen.radiantly172@passinbox.com>
2024-11-19 08:53:43 -0500

    initial api directory

---
+++ api/src/test/java/net/humdynsys/humdyncen/HumdyncenApplicationTests.java
@@ -0,0 +1,13 @@
+package net.humdynsys.humdyncen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumdyncenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
